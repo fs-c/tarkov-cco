5b84a81d43da5a5ab2da88afc839ea2b7199cd04
fsoc <root@fsoc.space>
2025-01-30 19:08:23 +0100

    Tiny refactor & style touchups

--- src/index.tsx
+++ src/index.tsx
@@ -26,6 +26,7 @@ export function App() {
         ItemType.armor,
         ItemType.rig,
         ItemType.backpack,
+        ItemType.keys,
     ]);
 
     useEffect(() => {
@@ -58,7 +59,8 @@ export function App() {
 
     const maxSolutionItems = useSignal(5);
     const minSolutionBasePrice = useSignal(400000);
-    const numberOfSolutions = useSignal(10);
+
+    const numberOfSolutions = useSignal(30);
 
     const solutions = useSignal<{ item: ItemMetadata; count: number }[][]>([]);
 
@@ -78,109 +80,26 @@ export function App() {
     });
 
     return (
-        <div
-            className={
-                'min-h-screen min-w-screen bg-topography flex flex-row-reverse'
-            }
-        >
-            <div
-                className={'bg-stone-900 overflow-y-auto max-h-screen shrink-0'}
-            >
-                <table
-                    className={
-                        'table-auto w-max relative border-separate border-spacing-0'
-                    }
-                >
-                    <thead className={'text-left text-stone-600 top-0'}>
-                        <tr
-                            className={
-                                '*:font-normal *:border-b *:border-stone-800 *:pb-2 *:pt-4 *:sticky *:top-0 *:bg-stone-900/90 *:z-10'
-                            }
-                        >
-                            <th className={'px-2 pl-8'}></th>
-                            <th className={'px-2'}></th>
-                            <th className={'px-4'}>Base</th>
-                            <th className={'px-4'}>Flea</th>
-                            <th className={'px-4 pr-8'}>Diff</th>
-                        </tr>
-                    </thead>
-
-                    <tbody className={'text-stone-300 w-max'}>
-                        {filteredItemsWithDiff.value.map((item) => (
-                            <tr className={'*:py-2 w-max'}>
-                                <td className={'px-2 pl-8'}>
-                                    <div
-                                        className={
-                                            'h-12 w-12 overflow-hidden rounded-md border border-stone-700'
-                                        }
-                                    >
-                                        <img
-                                            className={
-                                                'scale-[1.05] [clip-path:inset(1px)]'
-                                            }
-                                            src={item.iconLink ?? ''}
-                                        />
-                                    </div>
-                                </td>
-                                <td className={'px-2 w-max'}>
-                                    <p>{item.name}</p>
-                                    <p className={'text-stone-500'}>
-                                        {item.types
-                                            .map(getPrettyItemType)
-                                            .filter(Boolean)
-                                            .join(', ')}
-                                    </p>
-                                </td>
-                                <td className={'px-4'}>
-                                    {formatPrice(item.basePrice)}
-                                </td>
-                                <td className={'px-4'}>
-                                    {formatPrice(item.lastLowPrice)}
-                                </td>
-                                <td className={'px-4 pr-8'}>
-                                    {formatPrice(item.diff)}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-
-            <div className={'px-8 py-8 flex flex-col gap-16'}>
-                <div className={'flex flex-row gap-4 flex-wrap'}>
+        <div className='min-h-screen min-w-screen bg-topography flex flex-row text-stone-300'>
+            <div className='flex flex-col gap-16 overflow-y-auto max-h-screen'>
+                <div className='px-8 py-8 flex flex-row gap-4 flex-wrap'>
                     {solutions.value.map((solution) => (
-                        <div
-                            className={
-                                'flex flex-col gap-4 bg-stone-900 p-4 rounded-md border-stone-800'
-                            }
-                        >
-                            <div className={'flex flex-row gap-4'}>
+                        <div className='flex flex-col gap-4 bg-stone-900 p-4 rounded-md border-stone-800'>
+                            <div className='flex flex-row gap-4'>
                                 {solution.map(({ count, item }) => (
                                     <div className={'relative'}>
-                                        <div
-                                            className={
-                                                'overflow-hidden rounded-md border border-stone-700'
-                                            }
-                                        >
+                                        <div className='overflow-hidden rounded-md border border-stone-700'>
                                             <img
                                                 src={item.iconLink}
-                                                className={
-                                                    'w-16 h-16 scale-[1.05] [clip-path:inset(1px)]'
-                                                }
+                                                className='w-16 h-16 scale-[1.05] [clip-path:inset(1px)]'
+                                                alt={item.name}
+                                                title={item.name}
                                             />
                                         </div>
 
                                         {/* this is completely insane but otherwise the count didn't look centered */}
-                                        <div
-                                            className={
-                                                'absolute bottom-0 right-0 aspect-square bg-stone-300 text-stone-900 rounded-full translate-x-1/2 translate-y-1/2 h-5 text-center text-sm font-semibold'
-                                            }
-                                        >
-                                            <div
-                                                className={
-                                                    'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'
-                                                }
-                                            >
+                                        <div className='absolute bottom-0 right-0 aspect-square bg-stone-300 text-stone-900 rounded-full translate-x-1/2 translate-y-1/2 h-5 text-center text-sm font-semibold'>
+                                            <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
                                                 {count}
                                             </div>
                                         </div>
@@ -188,11 +107,7 @@ export function App() {
                                 ))}
                             </div>
 
-                            <div
-                                className={
-                                    'flex flex-row gap-4 w-full text-stone-300'
-                                }
-                            >
+                            <div className='flex flex-row gap-4 w-full text-stone-300'>
                                 <p>
                                     {solution.reduce(
                                         (acc, { count, item }) =>
@@ -214,6 +129,40 @@ export function App() {
                         </div>
                     ))}
                 </div>
+            </div>
+
+            <div className='bg-stone-900 overflow-y-auto max-h-screen shrink-0 inline-grid grid-cols-[auto_auto_auto_auto_auto] gap-4'>
+                <div className='grid grid-cols-subgrid col-span-full px-4 py-4 text-stone-600 border-b border-stone-800 sticky top-0 bg-stone-900 z-10'>
+                    <div className='col-span-2'></div>
+                    <div>Base</div>
+                    <div>Flea</div>
+                    <div>Diff</div>
+                </div>
+
+                {filteredItemsWithDiff.value.map((item) => (
+                    <div className='grid grid-cols-subgrid col-span-full px-4'>
+                        <div className='h-12 w-12 overflow-hidden rounded-md border border-stone-700'>
+                            <img
+                                className='scale-[1.05] [clip-path:inset(1px)]'
+                                src={item.iconLink ?? ''}
+                                alt={item.name}
+                                title={item.name}
+                            />
+                        </div>
+                        <div className='w-max'>
+                            <p>{item.name}</p>
+                            <p className='text-stone-500'>
+                                {item.types
+                                    .map(getPrettyItemType)
+                                    .filter(Boolean)
+                                    .join(', ')}
+                            </p>
+                        </div>
+                        <div>{formatPrice(item.basePrice)}</div>
+                        <div>{formatPrice(item.lastLowPrice)}</div>
+                        <div>{formatPrice(item.diff)}</div>
+                    </div>
+                ))}
             </div>
         </div>
     );
